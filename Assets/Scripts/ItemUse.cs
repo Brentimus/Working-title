0e730a184c0cb7d660fd2d99a45c7cf40d98d029
Brentimus <Gamepopins@gmail.com>
2022-11-03 19:15:29 +1000

    Game logic

--- Assets/Scripts/ItemUse.cs
+++ Assets/Scripts/ItemUse.cs
@@ -8,11 +8,19 @@ public class ItemUse : MonoBehaviour, IInteractable
     public string UnlockItem;
 
     private GameObject inventory;
+    public GameObject endScene;
+    public GameObject key_yellow;
     // Start is called before the first frame update
     void Start()
     {
         inventory = GameObject.Find("Inventory");
     }
+
+    public void end()
+    {
+        endScene.SetActive(true);
+    }
+
     public void Interact(DisplayImage currentDisplay)
     {
         if (inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem)
@@ -20,6 +28,14 @@ public class ItemUse : MonoBehaviour, IInteractable
             inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.GetComponent<Slot>().ClearSlot();
             Destroy(gameObject);
             Debug.Log("Use " + UnlockItem);
+            if (UnlockItem == "key_yellow")
+            {
+                end();
+            }
+            if (UnlockItem == "key_green")
+            {
+                key_yellow.SetActive(true);
+            }
         }
     }
 }
